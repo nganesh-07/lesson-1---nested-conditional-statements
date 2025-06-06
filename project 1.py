6988f766ffde8b06e6c260709bdcde44af69787f
nganesh <nganesh@iu.edu>
2025-06-06 09:27:49 -0700

    project 1

---
+++ project 1.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age here:"))
+
+if age <=10:
+    print("no")
+elif age>10:
+    print("yes")
+elif age<20:
+    print("yes")
+elif age >=20:
+    print("no")
+else:
+    print("input is invalid")
